94d39491d86556b963e2ea64803382a54b67970d
pcradu <148838351+pcradu@users.noreply.github.com>
2023-11-30 22:55:34 -0500

    Update TestSelectionSort.java

--- TestSelectionSort.java
+++ TestSelectionSort.java
@@ -6,33 +6,6 @@ import org.junit.jupiter.api.Test;
 
 class TestSelectionSort {
 
-    @Test
-    void testPositiveIntegers() {
-        SelectionSort sorter = new SelectionSort();
-        int[] testPositives = {4, 2, 7, 1, 9};
-        int[] expected = {1, 2, 4, 7, 9};
-
-        System.out.println("Running testPositiveIntegers:");
-        System.out.println("Original Array: " + arrayToString(testPositives));
-        int[] result = sorter.basicSelectionSort(testPositives);
-        System.out.println("Sorted Array: " + arrayToString(result));
-
-        assertArrayEquals(expected, result);
-    }
-
-    @Test
-    void testNegativeIntegers() {
-        SelectionSort sorter = new SelectionSort();
-        int[] testNegatives = {-3, -8, -1, -5, -2};
-        int[] expected = {-8, -5, -3, -2, -1};
-
-        System.out.println("Running testNegativeIntegers:");
-        System.out.println("Original Array: " + arrayToString(testNegatives));
-        int[] result = sorter.basicSelectionSort(testNegatives);
-        System.out.println("Sorted Array: " + arrayToString(result));
-
-        assertArrayEquals(expected, result);
-    }
 
     @Test
     void testMixedIntegers() {
